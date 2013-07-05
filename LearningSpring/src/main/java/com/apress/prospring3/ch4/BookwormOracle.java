7a711ab05f8a9a2029537a866a3e41ceb5e468fd
rumen84 <rumen_tod@abv.bg>
2013-07-05 08:20:27 +0300

    Testing Spring collections in with XML bean configuration

---
+++ LearningSpring/src/main/java/com/apress/prospring3/ch4/BookwormOracle.java
@@ -0,0 +1,10 @@
+package com.apress.prospring3.ch4;
+
+public class BookwormOracle implements Oracle {
+
+	@Override
+	public String defineMeaningOfLife() {
+		return "Encyclopedias are a waste of money - use the Internet";
+	}
+
+}
--- LearningSpring/src/main/java/org/rumen/test/Test.java
+++
@@ -1,5 +0,0 @@
-package org.rumen.test;
-
-public class Test {
-
-}
